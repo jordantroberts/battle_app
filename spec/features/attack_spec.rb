6907c35eeba457992519a5179619dfd347f374ea
Jordan <jordanroberts@talk21.com>
2019-04-17 16:29:44 +0100

    Attack Player 2 with form

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,7 @@
 feature 'Attack' do
   scenario 'attack player 2' do
     sign_in_and_play
-    click_link 'Attack'
+    click_button 'Attack'
     expect(page).to have_content 'Dave attacked Mittens'
   end
 end
